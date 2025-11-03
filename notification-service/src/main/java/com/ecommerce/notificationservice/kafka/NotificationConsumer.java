dd04a3a3c6c69968ecbf748a9a247de30d6b4c43
Amine Ait Benmessaoud <80128117+AmineAitBenmessaoud@users.noreply.github.com>
2025-11-03 14:53:50 +0100

    feat: Notification service working

--- notification-service/src/main/java/com/ecommerce/notificationservice/kafka/NotificationConsumer.java
+++ notification-service/src/main/java/com/ecommerce/notificationservice/kafka/NotificationConsumer.java
@@ -1,4 +1,24 @@
 package com.ecommerce.notificationservice.kafka;
 
+import com.ecommerce.notificationservice.model.Notification;
+import com.ecommerce.notificationservice.repository.NotificationRepository;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationConsumer {
+    private final NotificationRepository repository;
+
+    public NotificationConsumer(NotificationRepository repository) {
+        this.repository = repository;
+    }
+
+    @KafkaListener(topics = "payment-events", groupId = "notification-service")
+    public void consumePaymentEvent(String message) {
+        System.out.println("Received payment event: " + message);
+
+        Notification notification = new Notification();
+        notification.setMessage(message);
+        repository.save(notification);
+    }
 }
--- notification-service/src/main/java/com/ecommerce/notificationservice/model/Notification.java
+++ notification-service/src/main/java/com/ecommerce/notificationservice/model/Notification.java
@@ -1,4 +1,23 @@
 package com.ecommerce.notificationservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "notifications")
 public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
